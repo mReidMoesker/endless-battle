3f8fa4c1fb61391868667047a1dd7dc68fdb460c
mReidMoesker <opqetch@gmail.com>
2024-11-26 12:11:49 -0600

    functions III

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,93 +7,86 @@ let enemyImage = './assets/img/sprite/nothing.png'
 let enemyDMG = 0;
 let playerHP = 100;
 
-function addKarl() {
-  let enemyHP = 50;
-  const rand = [8, 10, 15];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Karl.png';
+const speak = select('.speak');
+
+'use strict';
+
+function wait(ms){
+  var start = new Date().getTime();
+  var end = start;
+  while(end < start + ms) {
+    end = new Date().getTime();
+ }
+}
+
+function addEnemy(hp, damageArray, imagePath) {
+  enemyHP = hp;
+  enemyDMG = damageArray[Math.floor(Math.random() * damageArray.length)];
+  enemyImage = imagePath;
   enemyBattle();
+}
+
+function addKarl() {
+  addEnemy(50, [8, 10, 15], './assets/img/sprites/Karl.png');
+  speak.innerText('KARL APPEARS!');
 }
 
 function addChort() {
-  let enemyHP = 10;
-  const rand = [2, 5, 8];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Chort.png'
-  enemyBattle();
+  addEnemy(10, [2, 5, 8], './assets/img/sprites/Chort.png');
+  speak.innerText('A CHORT APPEARS');
 }
 
 function addHippy() {
-  let enemyHP = 20;
-  const rand = [1, 2, 3];
-  let chance = Math.floor(Math.random(rand.length) * 10);
-  if (chance > 2) {enemyBattle()}
-  else {
-    hippyTalk();
-    getNewEnemy();
-  }
+  speak.innerText('A HIPPY APPEARS!');
+  wait(2000)
+  speak.innerText('"Peace and love, man!"');
+  giveHP();
 }
 
 function addGreg() {
-  let enemyHP = 40;
-  const rand = [5, 10, 12];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Greg.png'
-  enemyBattle();
+  addEnemy(40, [5, 10, 12], './assets/img/sprites/Greg.png');
+  speak.innerText('GREG APPEARS!');
 }
 
 function addSteve() {
-  let enemyHP = 40;
-  const rand = [5, 10, 12];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Greg.png'
-  enemyBattle();
+  addEnemy(40, [5, 10, 12], './assets/img/sprites/Greg.png');
+  speak.innerText('STEVE APPEARS!');
 }
 
 function addSlime() {
-  let enemyHP = 80;
-  const rand = [20, 25, 30];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Slime.png'
-  enemyBattle();
+  addEnemy(80, [20, 25, 30], './assets/img/sprites/Slime.png');
+  speak.innerText('AN ANGRY SLIME APPEARS!')
 }
 
 function addHound() {
-  let enemyHP = 15;
-  const rand = [10, 12, 15];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/hound.png'
-  enemyBattle();
+  addEnemy(15, [10, 12, 15], './assets/img/sprites/hound.png');
+  speak.innerText('A RABID DOG APPEARS!');
 }
 
 function addSpider() {
-  let enemyHP = 25;
-  const rand = [8, 10, 12];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Greg.png'
-  enemyBattle();
+  addEnemy(25, [8, 10, 12], './assets/img/sprites/Greg.png');
+  speak.innerText('A GIANT SPIDER APPEARS!')
 }
 
 function addPsycho() {
-  let enemyHP = 25;
-  const rand = [8, 10, 12];
-  let enemyDMG = Math.floor(Math.random(rand) * 10);
-  let enemyImage = './assets/img/sprites/Greg.png'
-  enemyBattle();
+  addEnemy(25, [8, 10, 12], './assets/img/sprites/Greg.png');
+  speak.innerText('A CRAZY PSYCHO APPEARS!');
 }
+
+function giveHP() {playerHP++ * 10}
 
 function getNewEnemy() {
   const rand = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
   let enemyChoice = Math.floor(Math.random(rand.length) * 10);
-  if (enemyChoice = 0) {addKarl()}
-  else if (enemyChoice = 1) {addChort()}
-  else if (enemyChoice = 2) {addHippy()} 
-  else if (enemyChoice = 3) {addGreg()} 
-  else if (enemyChoice = 4) {addSteve()}
-  else if (enemyChoice = 5) {addSlime()}
-  else if (enemyChoice = 6) {addHound()}
-  else if (enemyChoice = 7) {addSpider()}
-  else if (enemyChoice = 8) {addPsycho()}
+  if (enemyChoice === 0) {addKarl()}
+  else if (enemyChoice === 1) {addChort()}
+  else if (enemyChoice === 2) {addHippy()} 
+  else if (enemyChoice === 3) {addGreg()} 
+  else if (enemyChoice === 4) {addSteve()}
+  else if (enemyChoice === 5) {addSlime()}
+  else if (enemyChoice === 6) {addHound()}
+  else if (enemyChoice === 7) {addSpider()}
+  else if (enemyChoice === 8) {addPsycho()}
   else {giveHP()}
 }
 
@@ -109,10 +102,13 @@ function enemyBattle() {
 }
 
 function gamePlay() {
-  if (playerHP < 1) {endGame()} 
+  if (playerHP < 1) {gameOver()} 
   else {continueGame()}
 }
 
-function endGame() {
-  
+function gameOver() {
+  speak.innerText('GAME OVER');
+  enemyHP = 0;
+  enemyDMG = 0;
+  playerHP = 0;
 }
